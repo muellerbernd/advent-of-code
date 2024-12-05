2730914500bc29c55dfbafa34148463934c3c3b9
Bernd Müller <github@muellerbernd.de>
2024-12-05 22:14:16 +0100

    [add] day03: task2 solution

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -9,14 +9,19 @@ fn task1(input: &str) -> i32 {
     for cap in re.captures_iter(&input) {
         let m = cap.get(0).unwrap().as_str();
         let cleaned_input: String = m.replace(|c: char| !(c.is_digit(10) | (c == ',')), "");
-        let nums: Vec<i32> = cleaned_input.split(",").map(|s| s.parse::<i32>().unwrap()).collect();
+        let nums: Vec<i32> = cleaned_input
+            .split(",")
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect();
         result += nums.get(0).unwrap() * nums.get(1).unwrap();
     }
     result
 }
 
-fn task2(lines: &Vec<&str>) -> u32 {
-    5
+fn task2(input: &str) -> i32 {
+    let re = Regex::new(r"don\'t\(\)(.*?)do\(\)").unwrap();
+    let cleaned_input = re.replace_all(input, "");
+    task1(&cleaned_input)
 }
 
 fn main() {
@@ -29,6 +34,6 @@ fn main() {
 
     let task1_solution = task1(&raw_input);
     println!("task1 solution is {}", task1_solution);
-    //let task2_solution = task2(&raw_input.lines().collect());
-    //println!("task2 solution is {}", task2_solution);
+    let task2_solution = task2(&raw_input);
+    println!("task2 solution is {}", task2_solution);
 }
