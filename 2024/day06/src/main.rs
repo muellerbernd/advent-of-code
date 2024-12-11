84dfc8ef8e88b601e557b6ed0c9505037cf97dfd
Bernd Müller <github@muellerbernd.de>
2024-12-11 21:22:20 +0100

    [change] day06: task1 solution

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -1,14 +1,12 @@
 use std::fs::read_to_string;
 
-fn parse_input(input: &str) -> ((usize, usize), Vec<Vec<bool>>) {
-    let mut obstacle_map: Vec<Vec<bool>> = Vec::new();
+fn parse_input(input: &str) -> ((usize, usize), Vec<(i32, i32)>) {
+    let mut obstacle_list: Vec<(i32, i32)> = Vec::new();
     let mut start_point: (usize, usize) = (0, 0);
     for (i, line) in input.lines().map(|l| l.to_string()).into_iter().enumerate() {
-        let mut line_mask: Vec<bool> = Vec::new();
         for (j, c) in line.chars().enumerate() {
             match c {
-                '.' => line_mask.push(false),
-                '#' => line_mask.push(true),
+                '#' => obstacle_list.push((i as i32, j as i32)),
                 '^' => {
                     start_point.0 = i;
                     start_point.1 = j
@@ -16,37 +14,57 @@ fn parse_input(input: &str) -> ((usize, usize), Vec<Vec<bool>>) {
                 _ => println!("unknown char {}", c),
             }
         }
-        obstacle_map.push(line_mask);
     }
-    (start_point, obstacle_map)
+    (start_point, obstacle_list)
 }
 
-fn task1(start_point: (usize, usize), mut obstacle_map: Vec<Vec<bool>>) -> i32 {
-    println!("{:?}", start_point);
-    println!("{:?}", obstacle_map);
-    let mut curr_point: (usize, usize) = start_point;
-    let directions: Vec<(i8, i8)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
-    let grid_width: usize = obstacle_map[0].len();
-    let grid_height: usize = obstacle_map.len();
-    while curr_point.0 > 0
-        && curr_point.0 < grid_height
-        && curr_point.1 > 0
-        && curr_point.1 < grid_width
-    {}
-    5
+fn task1(start_point: (usize, usize), obstacle_list: Vec<(i32, i32)>, length: i32) -> i32 {
+    let (mut x, mut y): (i32, i32) = (start_point.0 as i32, start_point.1 as i32);
+    let directions: Vec<(i32, i32)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let mut dir_ind = 0;
+    let mut traversed_grid: Vec<Vec<bool>> = vec![vec![false; length as usize]; length as usize];
+    loop {
+        println!("{:?}", (x, y));
+        traversed_grid[x as usize][y as usize] = true;
+        let (next_x, next_y) = (x + directions[dir_ind].0, y + directions[dir_ind].1);
+        if next_x >= length || next_x < 0 || next_y >= length || next_y < 0 {
+            break;
+        }
+        if obstacle_list.contains(&(next_x, next_y)) {
+            dir_ind = (dir_ind + 1) % 4
+        } else {
+            (x, y) = (next_x, next_y)
+        }
+    }
+    //for l in traversed_grid.iter() {
+    //    println!("{:?}", l);
+    //}
+    //traversed_grid
+    //    .iter()
+    //    .map(|v| v.iter().map(|v| *v as i32).sum::<i32>())
+    //    .sum()
+    let mut result = 0;
+    for l in traversed_grid.iter() {
+        for v in l.iter() {
+            if *v {
+                result += 1;
+            }
+        }
+    }
+    result
 }
 //fn task2(updates: Vec<Vec<i32>>, rules_map: HashMap<i32, Vec<i32>>) -> i32 {
 //}
 
 fn main() {
-    //let file_path = "../inputs/aoc_06.txt";
-    let file_path = "test_input.txt";
+    let file_path = "../inputs/aoc_06.txt";
+    //let file_path = "test_input.txt";
 
     let raw_input: String =
         read_to_string(file_path).expect("Should have been able to read the file");
 
     let (start_point, obstacle_map) = parse_input(&raw_input);
-    let task1_solution = task1(start_point, obstacle_map);
+    let task1_solution = task1(start_point, obstacle_map, raw_input.lines().count() as i32);
     println!("task1 solution is {}", task1_solution);
     //let task2_solution = task2(updates.clone(), rules_map.clone());
     //println!("task2 solution is {}", task2_solution);
