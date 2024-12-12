be20b6b77857c5e0a93204e932b1ac5271c4b2e9
Bernd Müller <github@muellerbernd.de>
2024-12-12 18:26:53 +0100

    [change] day06: task2 done, test all fields in grid

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -40,8 +40,39 @@ fn task1(start_point: (usize, usize), obstacle_list: Vec<(i32, i32)>, length: i3
         .map(|v| v.iter().map(|v| *v as i32).sum::<i32>())
         .sum()
 }
-//fn task2(updates: Vec<Vec<i32>>, rules_map: HashMap<i32, Vec<i32>>) -> i32 {
-//}
+
+fn task2(start_point: (usize, usize), obstacle_list: Vec<(i32, i32)>, length: i32) -> i32 {
+    let directions: Vec<(i32, i32)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let mut loop_counter: i32 = 0;
+    for i in 0..length {
+        for j in 0..length {
+            let (mut x, mut y): (i32, i32) = (start_point.0 as i32, start_point.1 as i32);
+            let mut new_obstacle_list = obstacle_list.clone();
+            new_obstacle_list.push((i, j));
+            let mut dir_ind = 0;
+            let mut traversed_grid_directions: Vec<Vec<Vec<bool>>> =
+                vec![vec![vec![false; directions.len()]; length as usize]; length as usize];
+            loop {
+                if traversed_grid_directions[x as usize][y as usize][dir_ind as usize] {
+                    loop_counter += 1;
+                    break;
+                } else {
+                    traversed_grid_directions[x as usize][y as usize][dir_ind as usize] = true;
+                }
+                let (next_x, next_y) = (x + directions[dir_ind].0, y + directions[dir_ind].1);
+                if next_x >= length || next_x < 0 || next_y >= length || next_y < 0 {
+                    break;
+                }
+                if new_obstacle_list.contains(&(next_x, next_y)) {
+                    dir_ind = (dir_ind + 1) % 4
+                } else {
+                    (x, y) = (next_x, next_y)
+                }
+            }
+        }
+    }
+    loop_counter
+}
 
 fn main() {
     let file_path = "../inputs/aoc_06.txt";
@@ -50,9 +81,17 @@ fn main() {
     let raw_input: String =
         read_to_string(file_path).expect("Should have been able to read the file");
 
-    let (start_point, obstacle_map) = parse_input(&raw_input);
-    let task1_solution = task1(start_point, obstacle_map, raw_input.lines().count() as i32);
+    let (start_point, obstacle_list) = parse_input(&raw_input);
+    let task1_solution = task1(
+        start_point,
+        obstacle_list.clone(),
+        raw_input.lines().count() as i32,
+    );
     println!("task1 solution is {}", task1_solution);
-    //let task2_solution = task2(updates.clone(), rules_map.clone());
-    //println!("task2 solution is {}", task2_solution);
+    let task2_solution = task2(
+        start_point,
+        obstacle_list.clone(),
+        raw_input.lines().count() as i32,
+    );
+    println!("task2 solution is {}", task2_solution);
 }
