e387ee4e8c043bb8fadc3dbae4980fe86033ca5d
Bernd Müller <github@muellerbernd.de>
2025-12-01 09:04:32 +0100

    [change](day01): task02 done

--- 2025/day01/src/main.rs
+++ 2025/day01/src/main.rs
@@ -24,24 +24,24 @@ fn task1(ops: &Vec<i32>) -> i32 {
     }
     return passwd;
 }
-//
-// fn task2(lines: &Vec<&str>) -> usize {
-//     // let mut left: Vec<usize> = Vec::new();
-//     // let mut right: Vec<usize> = Vec::new();
-//     // for line in lines {
-//     //     let nums: Vec<usize> = line
-//     //         .split("   ")
-//     //         .map(|n| n.parse::<usize>().unwrap())
-//     //         .collect();
-//     //     left.push(*nums.get(0).unwrap());
-//     //     right.push(*nums.get(1).unwrap());
-//     // }
-//     // let similarity: Vec<usize> = left
-//     //     .iter()
-//     //     .map(|&e| e * right.iter().filter(|x| **x == e).count())
-//     //     .collect();
-//     // similarity.iter().sum()
-// }
+
+fn task2(ops: &Vec<i32>) -> i32 {
+    let mut dial: i32 = 50;
+    let mut passwd: i32 = 0;
+    // determine direction and number of steps
+    for op in ops.into_iter() {
+        let dir = if *op >= 0 { 1 } else { -1 };
+        let steps = op.abs() as i32;
+
+        for _ in 0..steps {
+            dial = (dial + dir).rem_euclid(100);
+            if dial == 0 {
+                passwd += 1;
+            }
+        }
+    }
+    return passwd;
+}
 
 fn main() {
     let file_path = "../inputs/aoc_01.txt";
@@ -49,12 +49,10 @@ fn main() {
 
     let raw_input: String =
         read_to_string(file_path).expect("Should have been able to read the file");
-    println!("input is {}", raw_input);
     let parsed_operations = parse_operations(&raw_input.lines().collect());
-    println!("parsed input is {:?}", parsed_operations);
 
     let task1_solution = task1(&parsed_operations);
     println!("task1 solution is {}", task1_solution);
-    // let task2_solution = task2(&raw_input.lines().collect());
-    // println!("task2 solution is {}", task2_solution);
+    let task2_solution = task2(&parsed_operations);
+    println!("task2 solution is {}", task2_solution);
 }
