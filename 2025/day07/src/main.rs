d81f3e625d9ac2022a15e717a217642adc6a790e
Bernd Müller <github@muellerbernd.de>
2025-12-07 18:29:42 +0100

    [add](day07): task02 done :)

--- 2025/day07/src/main.rs
+++ 2025/day07/src/main.rs
@@ -1,25 +1,5 @@
 use std::fs::read_to_string;
 
-// fn parse_input(input: &str) {
-//     let start_line = input.lines().collect::<Vec<_>>()[0];
-//     let start_y: usize = start_line.find("S").unwrap();
-//     let mut cur_beams: Vec<bool> = vec![false; start_line.len()];
-//     cur_beams[start_y] = true;
-//     println!("cur_beams {:?}", cur_beams);
-//     for (i, line) in input.lines().skip(1).enumerate() {
-//         println!("i {}, line {}", i, line);
-//         let splitters: Vec<_> = line.match_indices("^").collect();
-//         for (idx, _) in splitters {
-//             if cur_beams[idx] {
-//                 // split
-//                 cur_beams[idx] = false;
-//                 cur_beams[idx + 1] = true;
-//                 cur_beams[idx - 1] = true;
-//             }
-//         }
-//     }
-// }
-
 fn task1(input: &str) -> i64 {
     let start_line = input.lines().collect::<Vec<_>>()[0];
     let start_y: usize = start_line.find("S").unwrap();
@@ -41,7 +21,24 @@ fn task1(input: &str) -> i64 {
     return split_counter;
 }
 
-// fn task2(input: &str) -> i64 {}
+fn task2(input: &str) -> usize {
+    let start_line = input.lines().collect::<Vec<_>>()[0];
+    let start_y: usize = start_line.find("S").unwrap();
+    let mut cur_beams: Vec<usize> = vec![0; start_line.len()];
+    cur_beams[start_y] = 1;
+    for (i, line) in input.lines().skip(1).enumerate() {
+        let splitters: Vec<_> = line.match_indices("^").collect();
+        for (idx, v) in splitters {
+            if cur_beams[idx] > 0 {
+                // split
+                cur_beams[idx + 1] += cur_beams[idx];
+                cur_beams[idx - 1] += cur_beams[idx];
+                cur_beams[idx] = 0;
+            }
+        }
+    }
+    return cur_beams.iter().sum();
+}
 
 fn main() {
     let file_path = "../inputs/aoc_07.txt";
@@ -52,8 +49,8 @@ fn main() {
     // parse_input(&raw_input);
     let task1_solution = task1(&raw_input);
     println!("task1 solution is {}", task1_solution);
-    // let task2_solution = task2(&raw_input);
-    // println!("task2 solution is {}", task2_solution);
+    let task2_solution = task2(&raw_input);
+    println!("task2 solution is {}", task2_solution);
 }
 
 #[test]
@@ -62,6 +59,6 @@ fn test_input() {
     let raw_input = read_to_string(file_path).expect("Should have been able to read the file");
     let task1_solution = task1(&raw_input);
     assert_eq!(task1_solution, 21);
-    // let task2_solution = task2(&raw_input);
-    // assert_eq!(task2_solution, 3263827);
+    let task2_solution = task2(&raw_input);
+    assert_eq!(task2_solution, 40);
 }
