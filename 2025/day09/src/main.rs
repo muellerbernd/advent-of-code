75f4563bc0d6849a083863892b9ad3b96fa6c36f
Bernd Müller <github@muellerbernd.de>
2025-12-09 19:32:17 +0100

    [add](day09): task01 done

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -1,26 +1,37 @@
 use std::fs::read_to_string;
 
-fn parse_input(input: &str) -> Vec<(usize, usize)> {
+fn parse_input(input: &str) -> Vec<(i64, i64)> {
     input
         .lines()
         .map(|l| {
-            let t = l.split(",").collect::<Vec<&str>>();
-            (
-                t[0].parse::<usize>().unwrap(),
-                t[1].parse::<usize>().unwrap(),
-            )
+            let t = l
+                .split(",")
+                .map(|c| c.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>();
+            (t[0], t[1])
         })
         .collect()
 }
 
-fn task1(red_tiles: &Vec<(usize, usize)>) -> i64 {
-    println!("{:?}", red_tiles);
-    return 0;
+fn task1(red_tiles: &Vec<(i64, i64)>) -> i64 {
+    let mut areas: Vec<(i64, usize, usize)> = Vec::new();
+    for i in 0..red_tiles.len() {
+        for j in i + 1..red_tiles.len() {
+            let tile_a = red_tiles[i];
+            let tile_b = red_tiles[j];
+
+            let area = (tile_a.0 - tile_b.0 + 1).abs() * (tile_a.1 - tile_b.1 + 1).abs();
+            areas.push((area, i, j));
+        }
+    }
+    areas.sort_by(|(a, _, _), (b, _, _)| a.cmp(b));
+    let (d, _, _) = areas.last().unwrap();
+    return *d;
 }
 
 fn main() {
-    // let file_path = "../inputs/aoc_08.txt";
-    let file_path = "test_input.txt";
+    let file_path = "../inputs/aoc_09.txt";
+    // let file_path = "test_input.txt";
 
     let raw_input = read_to_string(file_path).expect("Should have been able to read the file");
 
@@ -31,16 +42,14 @@ fn main() {
     // println!("task2 solution is {}", task2_solution);
 }
 
-// #[test]
-// fn task01() {
-//     let file_path = "test_input.txt";
-//     let raw_input = read_to_string(file_path).expect("Should have been able to read the file");
-//     let junction_boxes = parse_input(&raw_input);
-//     let task1_solution = task1(&junction_boxes, 10);
-//     assert_eq!(task1_solution, 40);
-//     // let task2_solution = task2(&junction_boxes);
-//     // assert_eq!(task2_solution, 25272);
-// }
+#[test]
+fn task01() {
+    let file_path = "test_input.txt";
+    let raw_input = read_to_string(file_path).expect("Should have been able to read the file");
+    let red_tiles = parse_input(&raw_input);
+    let task1_solution = task1(&red_tiles);
+    assert_eq!(task1_solution, 50);
+}
 //
 // #[test]
 // fn task02() {
